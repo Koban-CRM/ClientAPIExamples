b3e43272580755d2547c8ae4bf3c4c06b378b0f3
guillaumeKoban <40759279+guillaumeKoban@users.noreply.github.com>
2021-08-25 15:57:58 +0200

    Adding Third Post and Get example

--- CSharpClient/Program.cs
+++ CSharpClient/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CSharpClient.Service;
+using NoCRM.Api.Client;
+using NoCRM.Api.ClientStructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,26 @@ namespace CSharpClient
     {
         static void Main(string[] args)
         {
+            string _apikey = "YOUR_KOBAN_API_KEY";
+            string _apiuri = "YOUR_KOBAN_SERVER";
+            string _username = "YOUR_KOBAN_USER_KEY";
+            ncToken tk = new ncToken() { ApiKey = _apikey, ApiUri = _apiuri, Username = _username };
+            ThirdService tsv = new ThirdService();
+
+            // Retrieve Accounts created or updated since 6 months
+            List<ncThird> thirds = tsv.GetUpdatedAccounts(tk, DateTime.Now.AddMonths(-6));
+
+            // Post 2 new accounts to Koban
+            ncThird t1 = new ncThird() { Extcode = "NEWACCOUNT1", Label = "New Account 1", EMail = "contact@newaccount1.fr" };
+            ncThird t2 = new ncThird() { Extcode = "NEWACCOUNT2", Label = "New Account 2", EMail = "contact@newaccount2.fr" };
+
+            List<ncThird> newthirds = new List<ncThird>();
+            newthirds.Add(t1);
+            newthirds.Add(t2);
+
+            List<string> res = tsv.Add(tk, newthirds);
+            t1.Guid = res[0];
+            t2.Guid = res[1];
         }
     }
 }
---
+++ CSharpClient/Service/ThirdService.cs
@@ -0,0 +1,60 @@
+﻿using NoCRM.Api.Client;
+using NoCRM.Api.ClientStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient.Service
+{
+    public class ThirdService
+    {
+        /// <summary>
+        /// Get Updated accounts since a date.
+        /// </summary>
+        /// <param name="tk">Koban API Token</param>
+        /// <param name="since">Reference Date</param>
+        /// <param name="key">Koban Key (Default extcode)</param>
+        /// <returns></returns>
+        public List<ncThird> GetUpdatedAccounts(ncToken tk, DateTime since, string key = "Extcode")
+        {
+            List<ncThird> res = new List<ncThird>();
+            int startindex = 0;
+            int maxlength = 0;
+            int paginationlength = 20;
+
+            ThirdRequest tr = new ThirdRequest(tk);
+            ncResultList<ncThird> kbnresult = tr.ListUpdated(since, "", paginationlength, startindex);
+            maxlength = kbnresult.Length;
+            res.AddRange(kbnresult.List);
+            startindex = startindex + paginationlength;
+            while(startindex < maxlength)
+            {
+                kbnresult = tr.ListUpdated(since, "", paginationlength, startindex);
+                res.AddRange(kbnresult.List);
+                startindex = startindex + paginationlength;
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Post several thirds to Koban
+        /// </summary>
+        /// <param name="tk">Koban Token</param>
+        /// <param name="newthirds">Thirds to post</param>
+        /// <param name="key">Koban Key (Default extcode)</param>
+        public List<string> Add(ncToken tk, List<ncThird> newthirds, string key = "Extcode")
+        {
+            ThirdRequest tr = new ThirdRequest(tk);
+            ncResult<List<string>> res = tr.BulkImport(newthirds, key);
+            if (res.Errors != null && res.Errors.Count > 0)
+                throw new Exception("Koban Post Error");
+            else
+            {
+                return res.Result;
+            }
+        }
+    }
+}
